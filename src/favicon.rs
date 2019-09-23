379896468ff89dae29f383e13264d06584a970e7
Gark Garcia <37553739+GarkGarcia@users.noreply.github.com>
2019-09-23 14:48:11 -0300

    Reduced the API surface of the `favicon` module.

--- src/favicon.rs
+++ src/favicon.rs
@@ -2,157 +2,60 @@ extern crate image;
 extern crate tar;
 
 use crate::{
-    ico::Ico, png_sequence::PngSequence, Error, Icon, PngEntry, Size, SourceImage, STD_CAPACITY,
+    png_sequence::PngSequence, Error, Icon, PngEntry, SourceImage, STD_CAPACITY
 };
 use image::DynamicImage;
-use resvg::usvg::{Tree, XmlOptions};
 use std::{
     fs::File,
     io::{self, Write},
     path::{Path, PathBuf},
+    cmp::{Ord, Ordering}
 };
 
-const SHORTCUT_SIZES: [Size; 3] = [Size(16), Size(32), Size(48)];
+macro_rules! path {
+    ($format: expr, $($arg: expr)*) => {
+        PathBuf::from(format!($format, $($arg)*))
+    };
+}
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 /// A comprehencive _favicon_ builder.
 pub struct FavIcon {
-    raw_sequence: PngSequence,
-    html_helper: Vec<u8>,
-    ms_tile_color: Option<Color>,
-    shortcut_icon: Option<Vec<u8>>,
-    safari_pinned_tab_icon: Option<String>,
+    internal: PngSequence,
+    entries: Vec<FavIconKey>
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-/// The _entry type_ for `FavIcon`.
-pub enum FavIconEntry {
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Ord)]
+/// The _key type_ for `FavIcon`.
+pub enum FavIconKey {
     /// Variant for 
-    /// _[Safari web-app icons](https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html)_.
-    AppleTouchIcon,
+    /// _[Apple touch icons](https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html)_.
+    AppleTouchIcon(u32),
     /// Variant for generic entries.
     Icon(u32),
-    /// Variant for configuring
-    /// _[IE app icons](https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/dn320426(v=vs.85))_.
-    MsApplicationIcon(Color),
 }
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
-/// A simple representation of an RGB color.
-pub struct Color(u8, u8, u8);
 
 impl FavIcon {
-    /// Adds an entry for a 
-    /// _[shortcut icon](https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/The_head_metadata_in_HTML#Adding_custom_icons_to_your_site)_
-    /// , creating a `.ico` file containing a _16x16_ entry, a _32x32_ entry and
-    /// a _48x48_ entry.
-    /// 
-    /// # Return Value
-    /// 
-    /// * Returns `Err(Error::Io(io::Error::from(io::ErrorKind::AlreadyExists)))`
-    ///   if the icon already contains a _shortcut icon_.
-    /// * Returns `Err(_)` if the construction of the `.ico` file fails or if the
-    ///   icon's html helper can't be updated.
-    /// * Returns `Ok(())` otherwise.
-    pub fn add_shortcut_icon<F: FnMut(&SourceImage, u32) -> DynamicImage>(
-        &mut self,
-        filter: F,
-        source: &SourceImage,
-    ) -> Result<(), Error<FavIconEntry>> {
-        if let Some(_) = self.shortcut_icon {
-            return Err(Error::Io(io::Error::from(io::ErrorKind::AlreadyExists)));
+    fn get_html_helper(&self) -> io::Result<Vec<u8>> {
+        let mut helper = Vec::with_capacity(self.entries.len() * 90);
+
+        for entry in &self.entries {
+            write!(
+                helper,
+                "<link rel=\"{0}\" type=\"image/png\" sizes=\"{1}x{1}\" href=\"{2}\">\n",
+                entry.rel(), entry.as_ref(), entry.to_path_buff().display()
+            )?;
         }
 
-        let mut ico = Ico::new();
-
-        if let Err(err) = ico.add_entries(filter, source, SHORTCUT_SIZES.to_vec()) {
-            match err {
-                Error::AlreadyIncluded(_) => panic!("This shouldn't happen."),
-                _ => return Err(err.map(|_| unreachable!())),
-            }
-        }
-
-        let mut shortcut = Vec::with_capacity(15_000);
-        ico.write(&mut shortcut)?;
-
-        self.shortcut_icon = Some(shortcut);
-        write!(
-            self.html_helper,
-            "\n<link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"icons/favicon.ico\"/>"
-        )
-        .map_err(|err| Error::Io(err))
-    }
-
-    /// Add an entry for a 
-    /// _[Safari pinned tab icon](https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/pinnedTabs/pinnedTabs.html)_.
-    /// 
-    /// # Return Value
-    /// 
-    /// * Returns `Err(Error::Io(io::Error::from(io::ErrorKind::AlreadyExists)))`
-    ///   if the icon already contains a _Safari pinned tab icon_.
-    /// * Returns `Err(_)` if the icon's html helper can't be updated.
-    /// * Returns `Ok(())` otherwise.
-    pub fn add_safari_pinned_tab_icon(
-        &mut self,
-        source: &Tree,
-        color: &Color,
-    ) -> Result<(), Error<FavIconEntry>> {
-        if let Some(_) = self.safari_pinned_tab_icon {
-            return Err(Error::Io(io::Error::from(io::ErrorKind::AlreadyExists)));
-        }
-
-        self.safari_pinned_tab_icon = Some(source.to_string(XmlOptions::default()));
-        write!(
-            self.html_helper,
-            "\n<link rel=\"mask-icon\" href=\"icons/safari-pinned-tab.svg\" color=\"{}\"/>",
-            color.to_hex()
-        )
-        .map_err(|err| Error::Io(err))
-    }
-
-    #[inline]
-    fn append_helper(&mut self, entry: &FavIconEntry) -> io::Result<()> {
-        match entry {
-            FavIconEntry::AppleTouchIcon => self.append_apple_helper(),
-            FavIconEntry::Icon(size) => self.append_icon_helper(*size),
-            FavIconEntry::MsApplicationIcon(color) => self.append_ms_app_helper(*color),
-        }
-    }
-
-    #[inline]
-    fn append_apple_helper(&mut self) -> io::Result<()> {
-        write!(
-            self.html_helper,
-            "\n<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"icons/apple-touch-icon.png\"/>"
-        )
-    }
-    #[inline]
-    fn append_icon_helper(&mut self, size: u32) -> io::Result<()> {
-        write!(
-            self.html_helper,
-            "\n<link rel=\"icon\" sizes=\"{0}x{0}\" type=\"image/png\" href=\"icons/favicon-{0}x{0}.png\"/>",
-            size
-        )
-    }
-    #[inline]
-    fn append_ms_app_helper(&mut self, color: Color) -> io::Result<()> {
-        self.ms_tile_color = Some(color);
-
-        write!(
-            self.html_helper,
-            "\n<meta name=\"msapplication-config\" href=\"icons/browserconfig.xml\">"
-        )
+        Ok(helper)
     }
 }
 
-impl Icon<FavIconEntry> for FavIcon {
+impl Icon<FavIconKey> for FavIcon {
     fn new() -> Self {
         FavIcon {
-            raw_sequence: PngSequence::new(),
-            html_helper: Vec::with_capacity(STD_CAPACITY * 90),
-            ms_tile_color: None,
-            shortcut_icon: None,
-            safari_pinned_tab_icon: None,
+            internal: PngSequence::new(),
+            entries: Vec::with_capacity(STD_CAPACITY)
         }
     }
 
@@ -160,44 +63,26 @@ impl Icon<FavIconEntry> for FavIcon {
         &mut self,
         filter: F,
         source: &SourceImage,
-        entry: FavIconEntry,
-    ) -> Result<(), Error<FavIconEntry>> {
-        let label = PngEntry(*entry.as_ref(), entry.to_path_buff());
+        entry: FavIconKey,
+    ) -> Result<(), Error<FavIconKey>> {
+        let path = entry.to_path_buff();
+        let png_entry = PngEntry(*entry.as_ref(), path);
 
-        if let Err(err) = self.raw_sequence.add_entry(filter, source, label) {
-            return Err(err.map(|_| entry));
+        if let Err(err) = self.internal.add_entry(filter, source, png_entry) {
+            Err(err.map(|_| entry))
+        } else {
+            let _ = self.entries.push(entry);
+            self.entries.sort();
+            Ok(())
         }
-
-        self.append_helper(&entry).map_err(|err| Error::Io(err))
     }
 
     fn write<W: Write>(&mut self, w: &mut W) -> io::Result<()> {
         let mut tar_builder = tar::Builder::new(w);
+        self.internal.write_to_tar(&mut tar_builder)?;
 
-        write_data(&mut tar_builder, self.html_helper.as_ref(), "helper.html")?;
-
-        if let Some(color) = self.ms_tile_color {
-            let browserconfig = get_ms_browserconfig(color);
-            write_data(
-                &mut tar_builder,
-                browserconfig.as_ref(),
-                "icons/browserconfig.xml",
-            )?;
-        }
-
-        if let Some(buff) = &self.shortcut_icon {
-            write_data(&mut tar_builder, buff.as_ref(), "icons/favicon.ico")?;
-        }
-
-        if let Some(svg) = &self.safari_pinned_tab_icon {
-            write_data(
-                &mut tar_builder,
-                svg.clone().into_bytes().as_ref(),
-                "icons/safari-pinned-tab.svg"
-            )?;
-        }
-
-        self.raw_sequence.write_to_tar(&mut tar_builder)
+        let helper = self.get_html_helper()?;
+        write_data(&mut tar_builder, helper.as_ref(), "helper.html")
     }
 
     fn save<P: AsRef<Path>>(&mut self, path: &P) -> io::Result<()> {
@@ -205,60 +90,50 @@ impl Icon<FavIconEntry> for FavIcon {
             let mut file = File::create(path.as_ref())?;
             self.write(&mut file)
         } else {
-            save_file(self.html_helper.as_ref(), path.as_ref(), "helper.html")?;
+            self.internal.save(path)?;
 
-            if let Some(color) = self.ms_tile_color {
-                let browserconfig = get_ms_browserconfig(color);
-
-                save_file(
-                    browserconfig.as_ref(),
-                    path.as_ref(),
-                    "icons/browserconfig.xml",
-                )?;
-            }
-
-            if let Some(buff) = &self.shortcut_icon {
-                save_file(buff.as_ref(), path.as_ref(), "icons/favicon.ico")?;
-            }
-
-            if let Some(svg) = &self.safari_pinned_tab_icon {
-                save_file(
-                    svg.clone().into_bytes().as_ref(),
-                    path.as_ref(),
-                    "icons/safari-pinned-tab.svg"
-                )?;
-            }
-
-            self.raw_sequence.save(path)
+            let helper = self.get_html_helper()?;
+            save_file(helper.as_ref(), path.as_ref(), "helper.html")
         }
     }
 }
 
-impl FavIconEntry {
+impl FavIconKey {
+    #[inline]
+    fn rel(&self) -> &str {
+        match self {
+            FavIconKey::AppleTouchIcon(_) => "apple-touch-icon",
+            FavIconKey::Icon(_) => "icon"
+        }
+    }
+
     #[inline]
     fn to_path_buff(self) -> PathBuf {
         match self {
-            FavIconEntry::AppleTouchIcon => PathBuf::from("icons/apple-touch-icon.png"),
-            FavIconEntry::Icon(size) => PathBuf::from(format!("icons/favicon-{0}x{0}.png", size)),
-            FavIconEntry::MsApplicationIcon(_) => PathBuf::from("icons/mstile-150x150.png"),
+            FavIconKey::AppleTouchIcon(size) => path!("icons/apple-touch-{0}x{0}-precomposed.png", size),
+            FavIconKey::Icon(size) => path!("icons/favicon-{0}x{0}.png", size),
         }
     }
 }
 
-impl AsRef<u32> for FavIconEntry {
+impl AsRef<u32> for FavIconKey {
     fn as_ref(&self) -> &u32 {
         match self {
-            FavIconEntry::AppleTouchIcon => &180,
-            FavIconEntry::Icon(size) => size,
-            FavIconEntry::MsApplicationIcon(_) => &150,
+            FavIconKey::AppleTouchIcon(size) | FavIconKey::Icon(size) => size
         }
     }
 }
 
-impl Color {
-    /// Display the color as _css-styled_ hex `String`.
-    fn to_hex(&self) -> String {
-        format!("#{:02x}{:02x}{:02x}", self.0, self.1, self.2)
+impl PartialOrd for FavIconKey {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self.as_ref().cmp(self.as_ref()) {
+            Ordering::Equal => match (self, other) {
+                (FavIconKey::AppleTouchIcon(_), FavIconKey::Icon(_)) => Some(Ordering::Greater),
+                (FavIconKey::Icon(_), FavIconKey::AppleTouchIcon(_)) => Some(Ordering::Less),
+                _ => Some(Ordering::Equal)
+            },
+            ord => Some(ord)
+        }
     }
 }
 
@@ -277,21 +152,4 @@ fn save_file(data: &[u8], base_path: &Path, path: &str) -> io::Result<()> {
     let mut file = File::create(path)?;
 
     file.write_all(data)
-}
-
-#[inline]
-fn get_ms_browserconfig(color: Color) -> Vec<u8> {
-    format!(
-        "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<browserconfig>
-    <msapplication>
-        <tile>
-            <square150x150logo src=\"mstile-150x150.png\"/>
-            <TileColor>#{}</TileColor>
-        </tile>
-    </msapplication>
-</browserconfig>",
-        color.to_hex()
-    )
-    .into_bytes()
 }

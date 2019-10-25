879d9b3eaabcb2cd3b6e13acaf878550a0f32acb
Pablo Escobar Gaviria <37553739+GarkGarcia@users.noreply.github.com>
2019-10-25 11:15:16 -0300

    Removed everything except the core infrastructure.

--- src/lib.rs
+++ src/lib.rs
@@ -95,28 +95,23 @@ pub extern crate image;
 pub extern crate resvg;
 
 use crate::usvg::Tree;
+pub use crate::error::{IconError, ResampleError};
 use image::{DynamicImage, GenericImageView, ImageError};
 pub use resvg::{raqote, usvg};
 use std::{
     convert::From,
-    error,
-    fmt::{self, Debug, Display, Formatter},
     fs::File,
     io::{self, Write},
     path::Path,
 };
 
-pub mod favicon;
-pub mod icns;
-pub mod ico;
 pub mod resample;
 pub mod encode;
+mod error;
 #[cfg(test)]
 mod test;
 
 const STD_CAPACITY: usize = 7;
-const MISMATCHED_DIM_ERR: &str =
-    "a resampling filter returned an image of dimensions other than the ones specified by it's arguments";
 
 /// A generic representation of an icon encoder.
 pub trait Icon
@@ -285,24 +280,6 @@ pub enum Image {
     Svg(Tree),
 }
 
-/// The error type for operations of the `Icon` trait.
-pub enum IconError<K: AsSize + Send + Sync> {
-    /// The `Icon` instance already includes an entry associated with this key.
-    AlreadyIncluded(K),
-    /// A resampling error.
-    Resample(ResampleError),
-}
-
-#[derive(Debug)]
-/// The error type for resampling operations.
-pub enum ResampleError {
-    /// Generic I/O error.
-    Io(io::Error),
-    /// A resampling filter produced results of dimensions
-    /// other the ones specified by it's arguments.
-    MismatchedDimensions(u32, (u32, u32)),
-}
-
 impl Image {
     /// Attempts to create a `Image` from a given path.
     ///
@@ -392,107 +369,3 @@ impl From<DynamicImage> for Image {
 
 unsafe impl Send for Image {}
 unsafe impl Sync for Image {}
-
-impl<K: AsSize + Send + Sync> IconError<K> {
-    /// Converts `self` to a `IconError<T>` using `f`.
-    pub fn map<T: AsSize + Send + Sync, F: FnOnce(K) -> T>(
-        self,
-        f: F
-    ) -> IconError<T> {
-        match self {
-            Self::AlreadyIncluded(e) => IconError::AlreadyIncluded(f(e)),
-            Self::Resample(err) => IconError::Resample(err),
-        }
-    }
-}
-
-impl<K: AsSize + Send + Sync> Display for IconError<K> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::AlreadyIncluded(_) => write!(
-                f,
-                "the icon already contains an entry associated with this key"
-            ),
-            Self::Resample(err) => <ResampleError as Display>::fmt(&err, f),
-        }
-    }
-}
-
-impl<K: AsSize + Send + Sync + Debug> Debug for IconError<K> {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            Self::AlreadyIncluded(e) => write!(f, "Error::AlreadyIncluded({:?})", e),
-            Self::Resample(err) => <ResampleError as Debug>::fmt(&err, f),
-        }
-    }
-}
-
-impl<K: AsSize + Send + Sync + Debug> error::Error for IconError<K> {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        if let Self::Resample(ref err) = self {
-            err.source()
-        } else {
-            None
-        }
-    }
-}
-
-impl<K: AsSize + Send + Sync> From<ResampleError> for IconError<K> {
-    fn from(err: ResampleError) -> Self {
-        Self::Resample(err)
-    }
-}
-
-impl<K: AsSize + Send + Sync> From<io::Error> for IconError<K> {
-    fn from(err: io::Error) -> Self {
-        Self::from(ResampleError::from(err))
-    }
-}
-
-impl<K: AsSize + Send + Sync> Into<io::Error> for IconError<K> {
-    fn into(self) -> io::Error {
-        if let Self::Resample(err) = self {
-            err.into()
-        } else {
-            io::Error::from(io::ErrorKind::InvalidInput)
-        }
-    }
-}
-
-impl From<io::Error> for ResampleError {
-    fn from(err: io::Error) -> Self {
-        Self::Io(err)
-    }
-}
-
-impl Display for ResampleError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Io(err) => write!(f, "{}", err),
-            Self::MismatchedDimensions(s, (w, h)) => write!(
-                f,
-                "{0}: expected {1}x{1}, got {2}x{3}",
-                MISMATCHED_DIM_ERR, s, w, h
-            ),
-        }
-    }
-}
-
-impl error::Error for ResampleError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        if let Self::Io(ref err) = self {
-            Some(err)
-        } else {
-            None
-        }
-    }
-}
-
-impl Into<io::Error> for ResampleError {
-    fn into(self) -> io::Error {
-        match self {
-            Self::Io(err) => err,
-            Self::MismatchedDimensions(_, _) => io::Error::from(io::ErrorKind::InvalidData),
-        }
-    }
-}
--- src/test.rs
+++ src/test.rs
@@ -1,14 +1,7 @@
-use crate::{
-    encode,
-    favicon::{self, Favicon},
-    icns::{self, Icns},
-    ico::{self, Ico},
-    resample, Icon, Image,
-};
+use crate::{encode, resample, Image};
 use std::{
     fs::File,
-    io::{self, BufWriter, Write},
-    path::Path,
+    io::{self, Write},
 };
 
 #[test]
@@ -35,95 +28,3 @@ fn test_resample() -> io::Result<()> {
 
     Ok(())
 }
-
-#[test]
-fn test_ico() {
-    let mut file = BufWriter::new(File::create("tests/test.ico").expect("Couldn't create file"));
-
-    let mut icon = Ico::new();
-    let img = Image::open("tests/hydra.png").expect("File not found");
-
-    let v = vec![ico::Key(32), ico::Key(64)];
-
-    if let Err(err) = icon.add_entries(resample::nearest, &img, v) {
-        panic!("{:?}", err);
-    }
-
-    // Should pass
-    if let Err(err) = icon.add_entry(resample::nearest, &img, ico::Key(128)) {
-        panic!("{:?}", err);
-    }
-
-    // Should fail
-    if let Ok(_) = icon.add_entry(resample::nearest, &img, ico::Key(32)) {
-        panic!("Should fail.");
-    }
-
-    if let Err(err) = icon.write(&mut file) {
-        panic!("{:?}", err);
-    }
-}
-
-#[test]
-fn test_icns() {
-    let mut file = BufWriter::new(File::create("tests/test.icns").expect("Couldn't create file"));
-
-    let mut icon = Icns::new();
-    let img = Image::open("tests/hydra.png").expect("Couldn't open file.");
-
-    let entries = vec![icns::Key::Rgba32, icns::Key::Rgba64];
-    if let Err(err) = icon.add_entries(resample::nearest, &img, entries) {
-        panic!("{:?}", err);
-    }
-
-    if let Err(err) = icon.add_entry(resample::nearest, &img, icns::Key::Rgba128) {
-        panic!("{:?}", err);
-    }
-
-    if let Err(err) = icon.write(&mut file) {
-        panic!("{:?}", err);
-    }
-}
-
-#[test]
-fn test_favicon() {
-    let path = Path::new("tests/favicon/");
-
-    let mut icon = Favicon::new();
-    let hydra = Image::open("tests/hydra.png").expect("Could not open `tests/hydra.png`.");
-    let bbox = Image::open("tests/box.svg").expect("Could not open `tests/box.svg`.");
-
-    let entries = vec![favicon::Key(32), favicon::Key(64)];
-
-    if let Err(err) = icon.add_entry(resample::nearest, &hydra, favicon::Key(16)) {
-        panic!("{:?}", err);
-    }
-
-    if let Err(err) = icon.add_entries(resample::cubic, &bbox, entries) {
-        panic!("{:?}", err);
-    }
-
-    if let Err(err) = icon.save(&path) {
-        panic!("{:?}", err);
-    }
-}
-
-/*fn test_png() {
-    let mut file = File::create("tests/test.tar").expect("Couldn't create file");
-
-    let mut icon = PngSequence::new();
-    let img = Image::open("tests/hydra.png").expect("File not found");
-
-    let entries = vec![
-        PngKey::from(32, "32/icon.png").unwrap(),
-        PngKey::from(64, "64/icon.png").unwrap(),
-    ];
-
-    if let Err(err) = icon.add_entries(resample::linear, &img, entries) {
-        panic!("{:?}", err);
-    }
-
-    if let Err(err) = icon.write(&mut file) {
-        panic!("{:?}", err);
-    }
-} */

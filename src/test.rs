138d7988379129cf5b86fecc9edbf3a836370da3
Pablo Escobar Gaviria <37553739+GarkGarcia@users.noreply.github.com>
2019-11-06 12:07:53 -0200

    Enhenced tests and fixed image parsing issues.

--- src/test.rs
+++ src/test.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 #[test]
-fn test_load() -> io::Result<()> {
+fn load() -> io::Result<()> {
     if let Err(err) = Image::load(File::open("tests/test.png")?) {
         panic!("FAILED AT PNG {:?}", err);
     }
@@ -34,25 +34,25 @@ fn test_load() -> io::Result<()> {
 }
 
 #[test]
-fn test_resample() -> io::Result<()> {
-    let mut file_near = File::create("tests/test_near.png").expect("Couldn't create file");
-    let mut file_linear = File::create("tests/test_linear.png").expect("Couldn't create file");
-    let mut file_cubic = File::create("tests/test_cubic.png").expect("Couldn't create file");
-    let mut file_svg = File::create("tests/test_svg.png").expect("Couldn't create file");
+fn rasterize() -> io::Result<()> {
+    let mut file_near = File::create("tests/rasterize/near.png").expect("Couldn't create file");
+    let mut file_linear = File::create("tests/rasterize/linear.png").expect("Couldn't create file");
+    let mut file_cubic = File::create("tests/rasterize/cubic.png").expect("Couldn't create file");
+    let mut file_svg = File::create("tests/rasterize/svg.png").expect("Couldn't create file");
 
-    let hydra = Image::open("tests/test.png").expect("File not found");
-    let box_svg = Image::open("tests/test.svg").expect("File not found");
+    let source_png = Image::open("tests/test.png").expect("File not found");
+    let source_svg = Image::open("tests/test.svg").expect("File not found");
 
-    let buf = encode::png(&hydra.rasterize(resample::nearest, 32).expect("Failed"))?;
+    let buf = encode::png(&source_png.rasterize(resample::nearest, 32).expect("Failed"))?;
     file_near.write_all(buf.as_ref())?;
 
-    let buf = encode::png(&hydra.rasterize(resample::linear, 32).expect("Failed"))?;
+    let buf = encode::png(&source_png.rasterize(resample::linear, 32).expect("Failed"))?;
     file_linear.write_all(buf.as_ref())?;
 
-    let buf = encode::png(&hydra.rasterize(resample::cubic, 32).expect("Failed"))?;
+    let buf = encode::png(&source_png.rasterize(resample::cubic, 32).expect("Failed"))?;
     file_cubic.write_all(buf.as_ref())?;
 
-    let buf = encode::png(&box_svg.rasterize(resample::nearest, 32).expect("Failed"))?;
+    let buf = encode::png(&source_svg.rasterize(resample::nearest, 32).expect("Failed"))?;
     file_svg.write_all(buf.as_ref())?;
 
     Ok(())
